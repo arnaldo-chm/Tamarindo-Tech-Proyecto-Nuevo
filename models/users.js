393443ccd9e30796592440205925251fd94a520a
cristopher-debug <carauza@ucenfotec.ac.cr>
2025-07-26 20:19:11 -0600

    registro mongo db

---
+++ models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const DB_URI = 'mongodb://localhost:27017/TamarindoTech'; 
+
+mongoose.connect(DB_URI,{})
+
+    .then(() => console.log("DB CONECTADA"))
+    .catch(err => console.log("Error de conexión:", err));
+
+//Modelo de usuario
+
+//Schema
+const userSchema = new mongoose.Schema({
+    nombre:{type:String, required:true},
+    correo:{type:String, required:true, unique:true},
+    password:{type:String, required:true},
+    telefono:{type:String, required:true},
+    tipoUsuario:{type:Number, default:0}, // Usuario 0 es comun, 1 es emprendedor, 2 es admin
+},{versionKey:false}) 
+
+
+let user = new mongoose.model('User',userSchema);
+
+module.exports = user;   
--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ app.set('view engine','ejs');//Motor de plantillas predeterminado
 app.use(express.static(path.join(__dirname,'public')));
 
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+//{"nombre": "steph","edad":25}
+//req.body.nombre -----steph
+//req.body.edad ---- 25
+app.use(bodyParser.urlencoded({extended:false}));
+
 //Rutas principales
 //nombre ruta, accion
 app.get('/',(req,res)=>{
@@ -59,7 +66,72 @@ app.get('/Admin_panel', (req, res) => {
     res.render('Admin_panel.html');
 });
 
+//Login
+const User = require('../models/users.js');
 
+app.post('/registrarse',(req,res)=>{
+    
+     let data = new User({
+        nombre:req.body.nombre,
+        correo:req.body.correo,
+        password:req.body.password,
+        telefono:req.body.telefono,
+        tipoUsuario:0
+    })
+
+    data.save()
+    .then(()=>{
+        console.log("Usuario registrado");
+        const resultado = { 
+            resultado: true,
+            mensaje: `usuario registrado con exito`}
+        res.json(resultado);
+    })
+
+    .catch(err => {
+        console.log("Error al guardar usuario:", err);
+        const resultado = { 
+            resultado: false,
+            mensaje: `Error al guardar usuario ${err}`}
+        res.json(resultado);
+    });
+
+    // console.log(req.body)
+    // console.log("llamada desde post logon")
+    //  res.json(resultado);
+})
+
+app.post('/authenticate',(req,res)=>{
+//paso 1: extarer la info de los usuarios
+    let data = {
+        correo:req.body.correo,
+        password:req.body.password
+    }
+
+    const existeUser = async()=>{
+        //paso 2: buscar el usuario en la base de datos
+        const usuario = await user.findOne({correo:data.correo});
+        //usuario {info usuario} si lo encuentra
+        //usuario null si no lo encuentra
+
+        if(usuario !=null){
+            if (data.password == usuario.password){
+                console.log("Usuario autenticado");
+                res.redirect('/'); //Redirigir a la página de inicio
+                
+            } else {
+                console.log("Contraseña incorrecta");
+                res.redirect('/login');
+            }
+        }else{
+            console.log("No existe el usuario");
+            res.redirect('/login');
+        }
+    }
+
+existeUser();
+
+})
 
 //Encender el server
 app.listen(3000,()=>{
